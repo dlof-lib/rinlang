263a8e4c0a18f7b17f4944497ddcde85a5e9f6cb
dlof-lib <javadlof8@gmail.com>
2026-07-25 05:39:39 +0300

    Initial commit from ZIP upload

---
+++ tools/test_containers.cpp
@@ -0,0 +1,69 @@
+#include "rin_lexer.h"
+#include "rin_parser.h"
+#include "rin_interpreter.h"
+#include <iostream>
+
+int main() {
+    std::string source = R"(
+        @container=my_data
+            text title = "بيانات ريـن";
+            print title;
+
+            Section=numbers
+                let a = 10;
+                let b = 4;
+                print Addition(a, b);
+                print Subtraction(a, b);
+                print Multiplication(a, b);
+                print Equal(a, b);
+            .end/Section
+
+            Translations
+                translation lang="ar" text="مرحبا";
+                translation lang="en" text="Hello";
+            .end/Translations
+
+            file path="data/output.rin";
+            installation my_data;
+            simplified save path="data/output.min.rin";
+        .end/container
+
+        @container=extra
+            text note = "بيانات إضافية";
+        .end/container
+
+        @container=my_data2
+            link to=my_data;
+            tying with=extra;
+            merge with=extra;
+        .end/container
+
+        @Containers.Group=my_group
+            @container=g1
+                text label = "عنصر داخل مجموعة";
+            .end/container
+        .end/Containers.Group
+
+        @Volume=vol1
+            @Containers.Group=g2
+                @container=c2
+                    text v = "داخل مجلد";
+                .end/container
+            .end/Containers.Group
+        .end/Volume
+    )";
+
+    try {
+        rin::Lexer lexer(source);
+        auto tokens = lexer.scanTokens();
+        rin::Parser parser(tokens);
+        auto statements = parser.parse();
+        rin::Interpreter interp;
+        std::string out = interp.run(statements);
+        std::cout << out;
+    } catch (rin::RinError& e) {
+        std::cout << "Parse/Lex error at line " << e.line << ": " << e.message << std::endl;
+        return 1;
+    }
+    return 0;
+}
