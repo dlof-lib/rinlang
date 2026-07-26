3a667e7875a3a72bfd674ab49d94af2284d3c47d
dlof-lib <javadlof8@gmail.com>
2026-07-26 15:45:45 +0300

    Initial commit from ZIP upload

---
+++ src/PackageIds.cs
@@ -0,0 +1,8 @@
+namespace RinLang.VSSDK
+{
+    internal static class PackageIds
+    {
+        public const int RinToolbarGroup = 0x1020;
+        public const int RunRinFileCommandId = 0x0100;
+    }
+}
