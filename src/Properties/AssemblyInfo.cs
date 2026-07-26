41e11b2ce35f1f494358465a5d08e507e9972224
dlof-lib <javadlof8@gmail.com>
2026-07-26 15:45:43 +0300

    Initial commit from ZIP upload

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Rin Language Support (VSSDK)")]
+[assembly: AssemblyDescription("Syntax classification, snippets, and a Run-File command for the Rin language in Visual Studio.")]
+[assembly: AssemblyCompany("rinlang")]
+[assembly: AssemblyProduct("RinLangVSSDK")]
+[assembly: AssemblyCopyright("MIT")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
