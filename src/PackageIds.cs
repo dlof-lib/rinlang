08333894ef475679f8ee068c3ba9513f6c4af28e
dlof-lib <javadlof8@gmail.com>
2026-07-31 22:00:22 +0300

    Initial commit from ZIP upload

--- src/PackageIds.cs
+++ src/PackageIds.cs
@@ -4,5 +4,7 @@ namespace RinLang.VSSDK
     {
         public const int RinToolbarGroup = 0x1020;
         public const int RunRinFileCommandId = 0x0100;
+        public const int BuildRinEngineCommandId = 0x0101;
+        public const int InsertLibraryCommandId = 0x0102;
     }
 }
